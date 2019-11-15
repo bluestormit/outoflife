61ce5a841c628c016624a3a3fcc77af94e5ab610
Conrado Basso <conradokronus@hotmail.com>
2019-11-15 15:07:34 -0300

    Add id user

--- ool_service_user/ool_service_user/ool_service_user/Models/User.cs
+++ ool_service_user/ool_service_user/ool_service_user/Models/User.cs
@@ -1,4 +1,5 @@
-﻿using MongoDB.Bson.Serialization.Attributes;
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
@@ -9,14 +10,18 @@ namespace ool_service_user.Models
 {
     public class User
     {
-        [BsonId]
         [JsonIgnore]
-        public string Id { get; set; }
+        public ObjectId Id { get; set; }
         [BsonRequired]
         public string Email { get; set; }
         [BsonRequired]
         public string Password { get; set; }
         [BsonRequired]
         public string Name { get; set; }
+        [JsonProperty]
+        public string IdUser
+        {
+            get => this.Id.ToString();
+        }
     }
 }
